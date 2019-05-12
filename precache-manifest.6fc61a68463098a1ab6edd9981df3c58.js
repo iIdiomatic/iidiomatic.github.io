56df3b088761afc298e32e47e1e7504202550929
iidiomatic <50513642+iIdiomatic@users.noreply.github.com>
2019-05-12 21:14:13 +0530

    publishing from react-redux-saga boilerplate

---
+++ precache-manifest.6fc61a68463098a1ab6edd9981df3c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ba8e158cb41c9ad85906b881568e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d266f3907e22c31d7386",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3875c3c4445d21802165",
+    "url": "/static/js/2.dd21da1a.chunk.js"
+  },
+  {
+    "revision": "d266f3907e22c31d7386",
+    "url": "/static/js/main.7199037d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
